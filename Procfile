1c51f79c6a4da1cef37c1d60e0ca1dfad2f370e7
JoaoNogaroli <70725289+JoaoNogaroli@users.noreply.github.com>
2023-01-05 10:50:56 -0300

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn -w 4 -l uvicorn.workers.UvicornWorker main:app
---
+++ main.py
@@ -0,0 +1,47 @@
+from fastapi import FastAPI
+# import matplotlib.pyplot as plt
+# from talib import abstract
+# import talib as tb
+# import MetaTrader5 as mt5
+from datetime import datetime
+# import pandas as pd
+import time
+import os
+import json
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/ativo/{teste}")
+async def root(teste):
+    lista = os.listdir(os.getcwd()+'/datas')
+    teste = teste.upper()
+    ativo = "df_"+teste
+    
+    x = list(map(str,lista))
+    y = "-".join(x)
+
+    if y.find(ativo) !=-1:
+        with open(os.getcwd()+'/datas'+'/'+ativo, 'r') as f:
+            data = json.load(f)
+
+        return {
+            'result':f"Yes, {ativo} exists in list",
+            'data':data
+        }
+    
+
+
+    else:
+        return {'result':f"No, {ativo}  does not exists in list"}
+
+    # for item in lista:
+    #     with open(os.getcwd()+'/datas'+'/'+item, 'r') as f:
+    #         data = json.load(f)
+    #         # df = pd.DataFrame(data)
+    #         return {'json_obj':data}
+   
