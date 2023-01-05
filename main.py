cdee0ab42f0a2953a700ad78153c8a485b0dfb38
JoaoNogaroli <70725289+JoaoNogaroli@users.noreply.github.com>
2023-01-05 11:13:40 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 # import matplotlib.pyplot as plt
 # from talib import abstract
 # import talib as tb
@@ -11,6 +13,17 @@ import json
 
 app = FastAPI()
 
+origins = [
+    ["*"]
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
